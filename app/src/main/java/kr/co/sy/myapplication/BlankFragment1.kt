a510139493c19e64e821a92f94c7901cb3e67f6a
JaekangLee <ksdy234@gmail.com>
2020-04-02 21:12:39 +0900

    UPDATE [ 바텀 네비게이션 사용 및 프래그먼트간의 전환 로직 수정 ] - FragmentManager / FragmentTransaction을 다루는 펑션 분리 - 현재 Fragment의 중복을 체크하는 펑션 생성 - 현재 프래그먼트와 전환할 페이지가 같다면 굳이 새로 생성하지 않음.  -> 현행 유지 - 등

--- app/src/main/java/kr/co/sy/myapplication/BlankFragment1.kt
+++ app/src/main/java/kr/co/sy/myapplication/BlankFragment1.kt
@@ -5,47 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment1.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
 class BlankFragment1 : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    var textTitleView: TextView? = null
+    var textContentsView: TextView? = null
+    lateinit var layoutView: View
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
+            textTitleView = layoutView.findViewById(R.id.text_title)
+            textContentsView = layoutView.findViewById(R.id.text_contents)
+
+            textTitleView?.text = param1
+            textContentsView?.text = param2
         }
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_blank1, container, false)
+        layoutView = inflater.inflate(R.layout.fragment_blank1, container, false)
+        return layoutView
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment1.
-         */
-        // TODO: Rename and change types and number of parameters
+        private  val ARG_PARAM1 = "param1"
+        private  val ARG_PARAM2 = "param2"
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             BlankFragment1().apply {
--- app/src/main/java/kr/co/sy/myapplication/BottomNaviActivity.kt
+++ app/src/main/java/kr/co/sy/myapplication/BottomNaviActivity.kt
@@ -7,19 +7,30 @@ import android.util.Log.d
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.activity_bottom_navi.*
+import java.util.*
 
-class BottomNaviActivity : AppCompatActivity() , View.OnClickListener{
+class BottomNaviActivity : AppCompatActivity(), View.OnClickListener {
 
-    var button1 : Button?= null
-    var button2 : Button?= null
-
+    var button1: Button? = null
+    var button2: Button? = null
+    var fm: FragmentManager? = null
+    var ft: FragmentTransaction? = null
+    var currentFragmentTag: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_navi)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment,BlankFragment1()).commit()
+        //
+        supportFragmentManager.let {
+            fm = it
+            ft = it.beginTransaction()
+
+        }
+
 
         button1 = findViewById(R.id.button1)
         button1?.setOnClickListener(this)
@@ -28,18 +39,60 @@ class BottomNaviActivity : AppCompatActivity() , View.OnClickListener{
         button2?.setOnClickListener(this)
     }
 
+    override fun onStart() {
+        super.onStart()
+        firstInitFragment()
+    }
+
     override fun onClick(v: View?) {
-
-        val ft = supportFragmentManager.beginTransaction()
-
-        when(v?.id){
-            R.id.button1-> {
-                ft.replace(R.id.fragment, BlankFragment1()).addToBackStack(null).commit()
+        when (v?.id) {
+            R.id.button1 -> {
+                replaceFragment(BlankFragment1.newInstance("블랭크1파람1", "블랭크1파람2"))
             }
-            R.id.button2-> {
-                ft.replace(R.id.fragment, BlankFragment2()).addToBackStack(null).commit()
+            R.id.button2 -> {
+                replaceFragment(BlankFragment2.newInstance("블랭크2파람1", "블랭크2파람2"))
             }
         }
 
     }
+
+    fun firstInitFragment() {
+        replaceFragment(BlankFragment1.newInstance("블랭크1파람1", "블랭크1파람2"))
+    }
+
+
+    fun replaceFragment(fragment: Fragment?) {
+        fragment?.let {
+            if (isCurrentFragmentEqual(it)) { //같은 화면을 눌렀을떈 새로 띄우는 일을 하지 않음
+
+            } else {
+                currentFragmentTag = fragment::class.java.simpleName
+                ft= fm?.beginTransaction()
+                ft?.replace(R.id.container, it)
+                ft?.addToBackStack(null)
+                ft?.commit()
+            }
+        }
+    }
+
+    fun isCurrentFragmentEqual(fragment: Fragment): Boolean { // 현재 프래그먼트의 태그와 바꾸길 원하는 프래그먼트의 태그를 비교함
+        currentFragmentTag?.let {
+            return currentFragmentTag == fragment::class.java.simpleName
+        } ?: run {
+            return false
+        }
+    }
+
+    override fun onBackPressed() {
+        fm?.let {
+            if (it.fragments.size > 1) {
+                super.onBackPressed()
+            } else {
+                finish()
+            }
+            return
+        }
+        super.onBackPressed()
+    }
+
 }
